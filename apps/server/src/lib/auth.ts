995ab1fa4dc826990617b3a04a1ab421ef23e0fc
Shilendra Singh <business.shilendra@gmail.com>
2025-10-20 09:29:00 +0530

    feat: implement authentication schema with user, session, account, and verification tables

--- apps/server/src/lib/auth.ts
+++ apps/server/src/lib/auth.ts
@@ -1,5 +1,10 @@
 import { betterAuth } from "better-auth"
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "@/db";
 export const auth = betterAuth({
+    database: drizzleAdapter(db, {
+        provider: 'pg',
+    }),
     socialProviders: { 
         github: { 
             clientId: process.env.GITHUB_CLIENT_ID!, 

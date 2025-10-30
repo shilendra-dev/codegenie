8a6a36449a947747ebf349bbc195071411915c08
Shilendra Singh <business.shilendra@gmail.com>
2025-10-30 13:03:12 +0530

    feat: create Input and Textarea components for form handling

--- apps/web/app/build/page.tsx
+++ apps/web/app/build/page.tsx
@@ -1,7 +1,12 @@
+import { Textarea } from "@/components/ui/textarea";
+
 export default function BuildPage() {
-    return (
-        <div className="flex w-full h-full items-center justify-center">
-            <h1 className="text-2xl font-semibold font-serif">{`Welcome to the Codegenie`}</h1>
-        </div>
-    );
+  return (
+    <div className="flex flex-col w-full h-full items-center justify-center">
+      <div className="flex gap-4 flex-col items-center">
+        <h1 className="text-2xl font-semibold font-serif">Welcome to Codegenie</h1>
+        <Textarea placeholder="Describe your project..." className="w-96 text-2xl"></Textarea>
+      </div>
+    </div>
+  );
 }
